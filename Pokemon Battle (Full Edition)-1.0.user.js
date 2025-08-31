27bab801e6bf3be8a00fbe1e6fdd26dbdfc97e3c
robbiestokes21 <dstokesncstudio@gmail.com>
2025-08-30 21:39:48 -0400

    Update Pokemon Battle (Full Edition)-1.0.user.js

--- Pokemon Battle (Full Edition)-1.0.user.js
+++ Pokemon Battle (Full Edition)-1.0.user.js
@@ -4,7 +4,7 @@
 // @connect     pokeapi.co
 // @connect     https://dstokesncstudio.com/pokeapi/pokeapi.php
 // @namespace   dstokesncstudio.com
-// @version     1.2.0.2
+// @version     1.3.0.0
 // @description Full version with XP, evolution, stats, sound, shop, battles, and walking partner — persistent across sites.
 // @include     *
 // @grant       GM.xmlHttpRequest
